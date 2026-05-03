9fdffc8dccef1f13808a92de73571250ac4f338e
Montenegro <mon24750@uvg.edu.gt>
2026-05-02 20:15:13 -0600

    feat: configurar cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const routes = require('./routes');
 const errorHandler = require('./middleware/errorHandler');
 
 const app = express();
 
+const corsOrigin = process.env.CORS_ORIGIN || '*';
+app.use(cors({ origin: corsOrigin }));
 app.use(express.json());
 app.use('/api', routes);
 app.use(errorHandler);
