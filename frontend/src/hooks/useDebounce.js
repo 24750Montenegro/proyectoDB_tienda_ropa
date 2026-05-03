c898fda0b1b1fceb840dc2a11008c40a2cdc2180
Montenegro <mon24750@uvg.edu.gt>
2026-05-03 17:26:51 -0600

    feat: agregar hook debounce

---
+++ frontend/src/hooks/useDebounce.js
@@ -0,0 +1,16 @@
+import { useEffect, useState } from 'react'
+
+export function useDebounce(value, delay = 300) {
+  const [debouncedValue, setDebouncedValue] = useState(value)
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setDebouncedValue(value)
+    }, delay)
+
+    // Cancela el timer anterior cuando el valor cambia.
+    return () => clearTimeout(timer)
+  }, [value, delay])
+
+  return debouncedValue
+}
