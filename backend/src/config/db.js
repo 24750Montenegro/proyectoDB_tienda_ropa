272ba3dc55136f8866c03f8aab2ebb9063d030ae
24750Montenegro <mon24750@uvg.edu.gt>
2026-05-18 08:42:14 -0600

    feat: implementación de conexión mediante sequelize en lugar de pool para utilizar metodos de ORM.

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,17 +1,29 @@
-const { Pool } = require('pg');
+const { Sequelize } = require('sequelize');
 
-const pool = new Pool({
-  // 'db' es el nombre del servicio en docker-compose
-  host: process.env.DB_HOST || 'db',
-  port: process.env.DB_PORT_INTERNAL || 5432,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  max: 10,
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST || 'db',
+    port: process.env.DB_PORT || 5432,
+    dialect: 'postgres',
+    logging: false,
+    pool: {
+      max: 7,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+    }
+  }
+)
 
-pool.on('error', (err) => {
-  console.error('Error inesperado en cliente del pool', err);
-});
+sequelize.authenticate()
+  .then(() => {
+    console.log('Conexión a la base de datos establecida exitosamente.');
+  })
+  .catch(err => {
+    console.error('No se pudo conectar a la base de datos:', err);
+  });
 
-module.exports = pool;
+module.exports = sequelize;
